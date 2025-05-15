aa057e8f6a9dee62f0463b91dab9c0ac6b714754
SrishtiiPandey <srishtip1313@gmail.com>
2025-05-15 18:12:47 +0530

    abc

--- src/main/java/org/java/models/Menu.java
+++ src/main/java/org/java/models/Menu.java
@@ -1,11 +1,12 @@
 package org.java.models;
+import org.java.enums.Base;
 import org.java.enums.Beverages;
 import org.java.enums.Ingredient;
 import org.java.enums.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Menu {
+public abstract class Menu {
 
     private int price;
     private List<Ingredient> topping = new ArrayList<>();
@@ -13,24 +14,36 @@ public class Menu {
     private List<Menu> menu = new ArrayList<>();
     private List<Meal> meal = new ArrayList<>();
 
-    private static List<Ingredient> ingredients = new ArrayList<>();
+    List<Ingredient> cheeseBurstIngredient = addIngredient(new Ingredient[]{Ingredient.Cheese,Ingredient.Jalepeo});
+    Pizza pizzaCheeseBurst = new Pizza(org.java.enums.Pizza.CheeseBurst, cheeseBurstIngredient, Base.WHEAT);
 
+    List<Ingredient> farmHouseIngredient = addIngredient(new Ingredient[]{Ingredient.Chicken, Ingredient.Cheese});
+    Pizza pizzaFarmHouse = new Pizza(org.java.enums.Pizza.Farmhouse, farmHouseIngredient, Base.MAIDA);
 
-    Pizza pizza = new Pizza(org.java.enums.Pizza.CheeseBurst, topping);
+    Meal cheeseBurstMeal = new Meal(pizzaCheeseBurst , Beverages.Coke);
+    Meal farmHouseMeal = new Meal(pizzaFarmHouse, Beverages.Mojito);
 
-
-    Meal meal = new Meal();
-
-    public static void main(String[] args) {
-        ingredients.add(Ingredient.Panner);
+    public List<Ingredient> addIngredient(Ingredient[] ingredients) {
+        List<Ingredient> ingred = new ArrayList<>();
+        for (Ingredient name : ingredients) {
+            ingred.add(name);
+        }
+        return ingred;
     }
+
+    public void addMeal(Meal meals){
+        meal.add(meals);
+    }
+
+
+
     //Constructor
     public Menu(List<Ingredient> topping, Type type, Pizza pizza, Beverages drinks) {
         this.price = type.getCost() + topping.stream().mapToInt(Ingredient::getCost).sum() + drinks.getCost() + pizza.getCost();
         this.topping = topping;
         this.type = type;
-        this.pizza = pizza;
-        this.drinks = drinks;
+//        this.pizza = pizza;
+//        this.drinks = drinks;
     }
 
     //Constructor
@@ -51,13 +64,14 @@ public class Menu {
     @Override
     public String toString() {
         return "--------- Bill ---------" +
-                "\nPizza         : " + pizza.getName().name() +
-                "\nBase          : " + pizza.getBase().name() +
-                "\nPizza Toppings: " + pizza.getIngredients().stream().map(Enum::name).toList() +
+//                "\nPizza         : " + pizza.getName().name() +
+//                "\nBase          : " + pizza.getBase().name() +
+//                "\nPizza Toppings: " + pizza.getIngredients().stream().map(Enum::name).toList() +
                 "\nSize (Type)   : " + type.name() +
                 "\nExtra Toppings: " + topping.stream().map(Enum::name).toList() +
-                "\nDrink         : " + drinks.name() +
+//                "\nDrink         : " + drinks.name() +
                 "\nTotal Price   : $" + price +
                 "\n------------------------------";
     }
 }
+
